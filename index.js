97265e7ae7204c278c12fe6e0cea088619b8f1ff
Turzo02 <naimul198650@gmail.com>
2025-11-09 18:03:54 +0600

    User DB created

--- index.js
+++ index.js
@@ -1,59 +1,67 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
-const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World! Server is running!')
-})
-
+app.get("/", (req, res) => {
+  res.send("Hello World! Server is running!");
+});
 
 //FreeLanceMarketPlace
 //lDEfPkqLCKUvJSDO
 
-const uri = "mongodb+srv://FreeLanceMarketPlace:lDEfPkqLCKUvJSDO@smartproduct.gqn7fwo.mongodb.net/?appName=SmartProducT";
-
+const uri =
+  "mongodb+srv://FreeLanceMarketPlace:lDEfPkqLCKUvJSDO@smartproduct.gqn7fwo.mongodb.net/?appName=SmartProducT";
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     await client.connect();
 
-    const jobsCollection = client.db('FreeLanceMarketPlace').collection('Jobs');
-   
+    const jobsCollection = client.db("FreeLanceMarketPlace").collection("Jobs");
+    const usersCollection = client
+      .db("FreeLanceMarketPlace")
+      .collection("Users");
 
-    app.get('/jobs', async(req, res) => {
-      const result = await jobsCollection.find().toArray();
+    //user apis
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
       res.send(result);
-    })
+    });
 
-
-
-
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const email = req.body.email;
+      const query = { email: email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "User already exists" });
+      }
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } 
-  
-  finally {
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
     // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
